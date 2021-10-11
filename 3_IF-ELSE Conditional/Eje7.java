b02e0eb04e600f35356ce2e2669fb2f012cab41f
ALeidaCarla <67081045+ALeidaCarla@users.noreply.github.com>
2021-10-11 16:11:55 -0400

    Add files via upload

---
+++ 3_IF-ELSE Conditional/Eje7.java
@@ -0,0 +1,21 @@
+
+/**
+ *Realm of Dragonfly era un proyecto de Juego que tenía pensado como proyecto.
+Algo que recuerdo, era que existía un item especial cuyo poder base es P. Este item es 
+especial, ya que solo puede dañar a enemigos con una cantidad de puntos de vida H, que 
+sea múltiplo de P.
+¿Puede el item de P puntos de poder, causar daño a un enemigo con una salud actual de H
+puntos de Vida?
+Condiciones de Contexto
+• P ≤ H
+• P y H son números enteros
+Datos de Entrada
+La entrada se proporciona por parámetros, en el siguiente orden:P H
+ * 
+ * @author (Aleida Eguivar) 
+ * @version (05/10/2021)
+ */
+public class Eje7
+{
+    
+}
---
+++ 3_IF-ELSE Conditional/Eje8.java
@@ -0,0 +1,24 @@
+/**
+ * Existe una familia de 5 personas… padre, madre y 3 hijos. Lo que pasa es que la señora de 
+casa, tiene problemas para recordar la edad de cada hijo y por ello no tiene bien claro quién 
+es el mayor, quien está en medio y quien es el menor. 
+Tu tarea es ayudarle, para ello debes tener en cuenta que cada hijo tiene un nombre S y 
+una edad N.
+Se te pide que indiques quien de los 3 es el mayor, quien es el menor y quien es el 
+hermano del medio.
+Condiciones de Contexto
+• Si es el nombre del i-esimo hermano
+• 0 < Ni < 100
+• Ni es un número entero. 
+• Las edades de los 3 hermanos siempre serán de valores distintos entre ellos.
+Datos de Entrada
+La entrada se proporciona por parámetros, en el siguiente orden: 
+
+ * 
+ * @author (Aleida Eguivar) 
+ * @version (05/10/2021)
+ */
+public class Eje8
+{
+    
+}
